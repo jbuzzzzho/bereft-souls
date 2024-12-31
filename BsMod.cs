cfe6ac6380f7c7032047a95c047e7b3fe7462874
Stephen T <xxlennygamerxx@gmail.com>
2024-12-30 22:06:20 -0600

    Initialize empty project.

---
+++ BsMod.cs
@@ -0,0 +1,8 @@
+using JetBrains.Annotations;
+
+using Terraria.ModLoader;
+
+namespace BereftSouls;
+
+[UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
+public sealed class BsMod : Mod;
