913f97d030323471b87174674b65fce679fe8e1a
Stephen T <xxlennygamerxx@gmail.com>
2025-01-28 01:13:13 -0600

    Remove old code.

--- src/FargosSotsPyramid/PyramidMod.cs
+++ src/FargosSotsPyramid/PyramidMod.cs
@@ -121,31 +121,9 @@ internal sealed class PyramidPlayer : ModPlayer
 
         Player.buffImmune[ModContent.BuffType<PharaohsCurse>()] = true;
     }
-}
 
-// The original plan was to allow tiles to be broken whenever, but we should
-// treat it like the dungeon.
-/*
-[UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
-internal sealed class PyramidGlobalTile : GlobalTile
-{
-    private static readonly Lazy<int[]> pyramid_tiles = new(
-        () =>
-        [
-            ModContent.TileType<PyramidSlabTile>(),
-            ModContent.TileType<PyramidRubbleTile>(),
-            ModContent.TileType<OvergrownPyramidTile>(),
-        ]
-    );
-
-    public override bool Slope(int i, int j, int type)
+    public override void PostUpdateBuffs()
     {
-        return pyramid_tiles.Value.Contains(type) || base.Slope(i, j, type);
-    }
-
-    public override bool CanKillTile(int i, int j, int type, ref bool blockDamaged)
-    {
-        return pyramid_tiles.Value.Contains(type) || base.CanKillTile(i, j, type, ref blockDamaged);
+        base.PostUpdateBuffs();
     }
 }
-*/
